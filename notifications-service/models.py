549c4b20fbb74a3402bb695e7c2fb80e3d6c852c
Suzana Tesovic <suzanatesovic03@gmail.com>
2026-05-14 20:11:53 +0200

    uradjen domaci

--- notifications-service/models.py
+++ notifications-service/models.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Notification(BaseModel):
     order_id: int
     product_id: int
     message: str
+    timestamp: Optional[str] = None
+    error_reason: Optional[str] = None
